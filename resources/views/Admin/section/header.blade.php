1f024e0cd1b94c4c87f2344c16ac4452881cf58c
AmirBahrami9 <amirbahraami99@gmail.com>
2020-03-17 11:56:53 +0330

    add admin panel view to the project

---
+++ resources/views/Admin/section/header.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">وبسایت</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+
+            <div class="navbar-left">
+                <a href="/logout" class="btn btn-sm btn-warning" style="margin: 15px">خروج از پنل کاربری</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li class="active"><a href="#">پنل اصلی</a></li>
+                <li><a href="#">صفحه کارمندان</a></li>
+                <li><a href="#">صفحه کاربران ثبت نام کرده</a></li>
+            </ul>
+            <ul class="nav nav-sidebar">
+                <li><a href="#">صفحه تعرفه های ثبت نام<span class="badge">0</span></a></li>
+                <li><a href="#">صفحه ثبت ملک<span class="badge">0</span></a></li>
+                <li><a href="#">صفحه ملک های ثبت شده از قبل<span class="badge">0</span></a></li>
+            </ul>
+            <ul class="nav nav-sidebar">
+                <li><a href="#">صفحه درخواست های ثبت شده از طریق صفحه اصلی</a></li>
+                <li><a href="#">صفحه ملک های ثبت شده کاربران از طریق صفحه اصلی</a></li>
+                <li><a href="#">*صفحه دریافت اعلام مغایرت ها و گزارش واگذاری ها</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ Route::get('/contact/login','UserController@signin')->name('signin');
 Route::post('/contact/login','UserController@login')->name('login');
 Route::get('/contact/signup','UserController@signup')->name('signup');
 Route::post('/contact/signup','UserController@register')->name('register');
+
+////route group for admin panel and management of it
+Route::prefix('admin')->namespace('Admin')->group(function (){
+    Route::get('/panel' , 'PanelController@index');
+
+});
