e883537515fe664b94eb617560e77d8cb9631797
arash karimadini <arashkarimadini@gmail.com>
2020-03-30 02:11:23 +0430

    change popup and error space and add signup page

--- public/base/jquery/DarbareMa.js
+++ public/base/jquery/DarbareMa.js
@@ -59,27 +59,6 @@ $(".pop-parent").show();
     document.documentElement.scrollTop = 0;
 	$(".body").css("overflow-y","hidden");
 });
-$(".pop-button-right4").on("click",function(){
-	$(".content4").hide();
-	$(".content3").show();
-	$(".circles div").css("background-color","white");
-	$(".circle2").css("background-color","#4016BA");
-	});
-$(".pop-button-left3").on("click",function(){
-	$(".content3").hide();
-	$(".content4").show();
-	$(".circles div").css("background-color","white");
-	$(".circle1").css("background-color","#4016BA");
-	});
-$(".pop-button-right3").on("click",function(){
-	$(".content3").hide();
-	$(".content2").show();
-	$(".input-center").css("display","none");
-	$(".pop-buttons2").css("margin-top","20%");
-	$(".pop-button-left2").text("دریافت کد تایید");
-	$(".circles div").css("background-color","white");
-	$(".circle3").css("background-color","#4016BA");
-	});
 $(".sex-content a").on("click",function(){
 	$(".sexbtn").text($(this).text());
 	});
@@ -88,17 +67,9 @@ $(".sexbtn").on("click",function(){
 	$(this).css("outline","none");
 	});
 $(".pop-button-left2").on("click",function(){
-	if($(this).text()==="ادامه"){
-		$(".content2").hide();
-		$(".content3").show();
-		$(".circles div").css("background-color","white");
-		$(".circle2").css("background-color","#4016BA");
-		}
-	else{
 	$(".input-center").css("display","block");
 	$(".pop-buttons2").css("margin-top","23%");
-	$(this).text("ادامه");
-	}
+	$(this).html('<a href="sabtNam.html" style="width:100%; display:block; text-decoration:none !important; color:white !important;">ادامه </a>');
 	});
 $(".sabt").on("click",function(){
 	$(".content1").hide();
@@ -114,8 +85,6 @@ $(".sabt").on("click",function(){
 	});
 $(".vorood").on("click",function(){
 	$(".content2").hide();
-	$(".content3").hide();
-	$(".content4").hide();
 	$(".content1").show();
 	$(this).addClass("pop-left-btn");
 	$(this).parent().addClass("pop-top-left");
@@ -151,7 +120,7 @@ $(".secProf").hover(function(){
 		color:"white"
 		});
 	},function(){
-	$(this).css({		
+	$(this).css({
 	color:"white !important",
 	background:"linear-gradient(90deg, rgba(190,86,212,1) 0%, rgba(64,22,186,1) 100%, rgba(64,22,186,1) 100%)"
 		});
@@ -169,7 +138,7 @@ $(".firstProf").hover(function(){
 		color:"white"
 		});
 	},function(){
-	$(this).css({		
+	$(this).css({
 	color:"white !important",
 	background:"linear-gradient(90deg, rgba(64,22,186,1) 0%, rgba(190,86,212,1) 100%, rgba(190,86,212,1) 100%)"
 		});
@@ -187,13 +156,13 @@ $(".navitems").hover(function(){
 		});
 	},function(){
 		if(!$(this).hasClass("home")){
-	$(this).css({		
+	$(this).css({
 	color:"white",
 	    boxُُSizing: "border-box",
         borderBottomColor:" rgba(0, 0, 0, 0)",
 		});}
 		});
-  
+
 $(window).on("scroll",function(){
 	var $height = $(window).scrollTop();
 	if($height > 20){
--- public/base/jquery/moshaverin.js
+++ public/base/jquery/moshaverin.js
@@ -60,27 +60,6 @@ $(".pop-parent").show();
     document.documentElement.scrollTop = 0;
 	$(".body").css("overflow-y","hidden");
 });
-$(".pop-button-right4").on("click",function(){
-	$(".content4").hide();
-	$(".content3").show();
-	$(".circles div").css("background-color","white");
-	$(".circle2").css("background-color","#4016BA");
-	});
-$(".pop-button-left3").on("click",function(){
-	$(".content3").hide();
-	$(".content4").show();
-	$(".circles div").css("background-color","white");
-	$(".circle1").css("background-color","#4016BA");
-	});
-$(".pop-button-right3").on("click",function(){
-	$(".content3").hide();
-	$(".content2").show();
-	$(".input-center").css("display","none");
-	$(".pop-buttons2").css("margin-top","20%");
-	$(".pop-button-left2").text("دریافت کد تایید");
-	$(".circles div").css("background-color","white");
-	$(".circle3").css("background-color","#4016BA");
-	});
 $(".sex-content a").on("click",function(){
 	$(".sexbtn").text($(this).text());
 	});
@@ -89,17 +68,9 @@ $(".sexbtn").on("click",function(){
 	$(this).css("outline","none");
 	});
 $(".pop-button-left2").on("click",function(){
-	if($(this).text()==="ادامه"){
-		$(".content2").hide();
-		$(".content3").show();
-		$(".circles div").css("background-color","white");
-		$(".circle2").css("background-color","#4016BA");
-		}
-	else{
 	$(".input-center").css("display","block");
 	$(".pop-buttons2").css("margin-top","23%");
-	$(this).text("ادامه");
-	}
+	$(this).html('<a href="sabtNam.html" style="width:100%; display:block; text-decoration:none !important; color:white !important;">ادامه </a>');
 	});
 $(".sabt").on("click",function(){
 	$(".content1").hide();
@@ -115,8 +86,6 @@ $(".sabt").on("click",function(){
 	});
 $(".vorood").on("click",function(){
 	$(".content2").hide();
-	$(".content3").hide();
-	$(".content4").hide();
 	$(".content1").show();
 	$(this).addClass("pop-left-btn");
 	$(this).parent().addClass("pop-top-left");
@@ -167,7 +136,7 @@ $('#carouselExampleControls').on('slid.bs.carousel', function () {
 $(".prev-btn").on("click",function(){
 	$(".carousel1").carousel('prev');
 	$(".carousel1").carousel('pause');
-	
+
 	if($(".first-slide").hasClass("active")){
 	  $(".number-btn").removeClass("firstNum");
 	$(".one").addClass("firstNum");
@@ -229,13 +198,13 @@ $(".metrazhbtn").on("click",function(){
 	$(".metrazh-content").slideToggle("slow");
 	$(this).css("outline","none");
 	$(".cost-content").slideUp("slow");
-	$(".mahale-content").slideUp("slow");	
+	$(".mahale-content").slideUp("slow");
 	});
 $(".costbtn").on("click",function(){
 	$(".cost-content").slideToggle("slow");
 	$(this).css("outline","none");
 	$(".metrazh-content").slideUp("slow");
-	$(".mahale-content").slideUp("slow");	
+	$(".mahale-content").slideUp("slow");
 	});
 $(".secProf").hover(function(){
 	$(this).css({
@@ -246,7 +215,7 @@ $(".secProf").hover(function(){
 		color:"white"
 		});
 	},function(){
-	$(this).css({		
+	$(this).css({
 	color:"white !important",
 	background:"linear-gradient(90deg, rgba(190,86,212,1) 0%, rgba(64,22,186,1) 100%, rgba(64,22,186,1) 100%)"
 		});
@@ -264,7 +233,7 @@ $(".firstProf").hover(function(){
 		color:"white"
 		});
 	},function(){
-	$(this).css({		
+	$(this).css({
 	color:"white !important",
 	background:"linear-gradient(90deg, rgba(64,22,186,1) 0%, rgba(190,86,212,1) 100%, rgba(190,86,212,1) 100%)"
 		});
@@ -282,13 +251,13 @@ $(".navitems").hover(function(){
 		});
 	},function(){
 		if(!$(this).hasClass("home")){
-	$(this).css({		
+	$(this).css({
 	color:"white",
 	    boxُُSizing: "border-box",
         borderBottomColor:" rgba(0, 0, 0, 0)",
 		});}
 		});
-  
+
 $(window).on("scroll",function(){
 	var $height = $(window).scrollTop();
 	if($height > 20){
